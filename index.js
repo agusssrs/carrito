35cf50ae5133a067959369a7baa6e3d3d445d223
agusssrs <agustinmancini2@hotmail.com>
2023-02-26 13:19:23 -0300

    commit

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const Perfumes = document.querySelector('.perfumes');
-const BuyBtn = document.querySelector('.BuyBtn');
-const MenuBurguer = document.querySelector('.menu-label');
-const ProductConfirm = document.querySelector('.product-confirm');
+// const Perfumes = document.querySelector('.perfumes');
+// const BuyBtn = document.querySelector('.BuyBtn');
+// const MenuBtn = document.querySelector('.menu-label');
+// const ProductConfirm = document.querySelector('.product-confirm');
 
-let cart = JSON.parse(LocalStorage.GetItem('cart')) || [];
-const SaveLocalStorage = (CartList) => {
-    LocalStorage.SetItem('cart', JSON.stringify(CartList));
-};
-const init = () => {
 
-}
-init();
+
+// let cart = JSON.parse(LocalStorage.GetItem('cart')) || [];
+// const SaveLocalStorage = (CartList) => {
+//     LocalStorage.SetItem('cart', JSON.stringify(CartList));
+// };
+
+// const MenuToggle = () =>{
+//     MenuBtn.classList.toggle('menu-toggle');
+//     if(cartMenu.classList.contains('open-cart')){
+//         cartMenu.classList.remove ('open-cart')
+//         return;
+//     }
+//     overlay.classList.toggle('show-overlay');
+// }
+
+// const init = () => {
+//     MenuBtn.addEventListener('click', MenuToggle);
+// }
+// init();
+
+
+const cartMenu = document.querySelector('cart')
+
+
+products.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('perfumes')
+    div.innerHTML = `
+    <div class="perfumes">
+        <div>
+            <img src="${producto.productImg}" alt="">
+            <h2>${producto.name} ${producto.ml} </h2>
+            <p>${producto.price}</p>
+            <a href="#" class="BuyBtn">Comprar</a>
+        </div>
+    </div>`
+});
+
+cartMenu.appendChild(div)
