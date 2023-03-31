e26e56f989d18a7dcb0c4f026c1c186fcac098e2
agusssrs <agustinmancini2@hotmail.com>
2023-03-31 18:16:17 -0300

    js

--- assets/data.js
+++ assets/data.js
@@ -1,49 +1,55 @@
 const products = [
     {
         id:1,
-        name: 'Jean Paul Gaultier Le Beau Le Parfum',
-        ml: '125',
-        price: '$30.500',
+        name: 'Le Beau Le Parfum',
+        brand: 'Jean Paul Gaultier',
+        ml: '125ml',
+        price: '30500',
         productImg: './assets/img/perfumes/142495-b-le-beau-le-parfum-edp-125ml_1.jpg'
     },
 
     {
         id:2,
-        name: 'Azzaro The Most Wanted Parfum',
-        ml: '100',
-        price: '$32.500',
+        name: 'The Most Wanted Parfum',
+        brand:'Azzaro',
+        ml: '100ml',
+        price: '32500',
         productImg: './assets/img/perfumes/azzaro-the-most-wanted-parfum-50ml.jpg'
     },
 
     {
         id:3,
-        name: 'Spicebomb Extreme',
-        ml: '90',
-        price: '$37.250',
+        name: 'Extreme',
+        brand: 'Spicebomb',
+        ml: '90ml',
+        price: '37250',
         productImg: './assets/img/perfumes/spicebombextreme.jpg'
     },
 
     {
         id:4,
-        name:'CH Bad Boy Cobalt EDP',
-        ml: '100',
-        price: '$29.650',
+        name:'Bad Boy Cobalt EDP',
+        brand: 'CH',
+        ml: '100ml',
+        price: '29650',
         productImg:'./assets/img/perfumes/142868-a-bad-boy-cobalt-edp-150ml_2.jpg',
     },
 
     {
         id:5,
-        name:'Yves Saint Laurent Y EDP',
-        ml: '100',
-        price: '$32.000',
+        name:'Y EDP',
+        brand: 'Yves Saint Laurent',
+        ml: '100ml',
+        price: '32000',
         productImg: './assets/img/perfumes/131161-a-y-edp-100ml_1_1.jpg',
     },
 
     {
         id:6,
-        name: 'Hugo Boss Bottled Night EDT',
-        ml: '100',
-        price: '$19.000',
+        name: 'Bottled Night EDT',
+        brand: 'Hugo Boss',
+        ml: '100ml',
+        price: '19000',
         productImg: './assets/img/perfumes/hugo-boss-boss-bottled-night-eau-de-toilette-200ml.jpg',
     }
 ];
--- index.js
+++ index.js
@@ -35,8 +35,8 @@ console.log(products)
      <div class="azzaro">
              <img src="${producto.productImg}" alt="">
              <h2>${producto.name} ${producto.ml} </h2>
-             <p>${producto.price}</p>
-             <a href="#" class="BuyBtn" id="${producto.id}">Comprar</a>
+             <p>$${producto.price}</p>
+             <a  class="BuyBtn" id="${producto.id}">Comprar</a>
      </div>`
      perfumesCards.appendChild(div)
      const btn = document.getElementById (`${producto.id}`)
@@ -55,8 +55,62 @@ console.log(products)
 
  ]
 
- const addItemCart = (prodId) => {
-     const item = products.find((prod) => prod.id === prodId)
-     cart.push(item)
-     console.log(cart, 'Cart')
- }
+
+const cartBox = () =>{
+   let itemBuy = '<h2>Tus perfumes!</h2>';
+   let total = 0;
+   cart.forEach(product => {
+    itemBuy += `
+    <div class="cart-item">
+                     <img src="${product.productImg}" alt="le beau">
+                     <div class="item-info">
+                         <h2 class="product-title">${product.name}</h2>
+                         <h3 class="product-brand">${product.brand}</h3>
+                         <p class="price">$${product.price}</p>
+                     </div>
+                     <div class="quantity-handler">
+                         <span class="quantity-minus">-</span>
+                         <span class="quantity-total">${product.cant}</span>
+                         <span class="quantity-plus">+</span>
+                     </div>
+                 </div>
+    `;     
+    total += parseInt(product.price);
+   });
+   itemBuy += `
+        <span class="end-cart-list"></span>
+        <div class="total-cart">
+            <p>Total:$</p>
+            <span>${total}</span>
+        </div>
+        <button class="btn-buy">Comprar</button>
+   `
+   document.getElementById ('cartBox').innerHTML = itemBuy;
+}
+
+const itemHandler = () =>{
+    let handler = document.querySelectorAll('.cart-item');
+    handler.forEach((p,i) => {
+        p[2][0].onclick= () =>{
+            let index = cart.findIndex((prod) => prod.name === p[1][0]);
+            cart[index].cant=cart[index].cant-1;
+        }
+    });
+} 
+
+const addItemCart = (prodId) => {
+    const item = products.find((prod) => prod.id === prodId)
+    cart.push({...item, cant:1});
+    console.log(cart, 'Cart')
+    localStorage.setItem ('CartItem', JSON.stringify(cart));
+    cartBox ();
+}
+
+const load = () =>{
+    if (localStorage.getItem('CartItem')!==null){
+        cart=JSON.parse(localStorage.getItem('CartItem'))
+    }
+    cartBox()
+}
+
+window.onload = load;
